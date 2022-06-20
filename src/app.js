4f006499c6f418d8b893366f12d1e861db74de7e
Dmitry Sazonov <seoweb@inbox.ru>
2022-06-21 00:44:36 +0300

    проверка хука нетлифи

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import './styles.scss'
 const root = document.getElementById('root')
 
 const context = {
-    title: "Заголовок",
+    title: "Заголовок 1",
     text: 'Пробный текст hook test 3'
 }
 const html = mainTpl(context)
