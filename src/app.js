0014f11a11c1b2a021301f1e5ef1d907cd2e9e67
Dmitry Sazonov <seoweb@inbox.ru>
2022-06-23 01:23:05 +0300

    компоненты

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
-import mainTpl from './layout/main/main.hbs'
+import './layout/main'
+import './layout/chat'
+
 import './components/button'
-import './styles.scss'
+import './components/error_code'
+import './components/form'
+import './components/form_buttons'
+import './components/form_row'
+import './components/heading'
+import './components/profile_avatar'
+import './components/profile_row'
+
+import './scss/common.scss'
 
 const root = document.getElementById('root')
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+let page
 
-const html = mainTpl({
-    title: 'Титл',
-    text: 'probe',
-    btn: {
-        text: 'ссыль',
-        link: '#12'
-    }
-})
+if (urlParams.has('page')) {
+    page = urlParams.get('page')
+} else {
+    page = "signIn"
+}
 
-root.innerHTML = html
+switch (page) {
+    case 'signIn':
+        console.log('signIn')
+        break
+    default:
+        console.log(page)
+}
